1811d79dbfcdaa6347eedd622ce3776847586dca
Luke Klymshyn <unseenmountain@gmail.com>
2019-11-03 21:38:15 -0800

    When you select an answer the next choice can't be selected only one answer can be selected per question

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var questions = {
+
+
+    Question :  "Where is Waldo currently?",
+    answerA : "The Arctic",
+    answerB : "Saturn",
+    answerC : "The Death Star",
+    answerD : "Doing his own thing, stop asking"
+
+}
