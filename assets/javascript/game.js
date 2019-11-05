8e755662d390ca9ac273e0f14763ba6b5790ed01
Luke Klymshyn <unseenmountain@gmail.com>
2019-11-04 19:21:13 -0800

    I got the users input to diplay if there answer is right or wrong

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,39 @@
-var questions = {
+// var questionsCorrect = 0;
 
 
-    Question :  "Where is Waldo currently?",
-    answerA : "The Arctic",
-    answerB : "Saturn",
-    answerC : "The Death Star",
-    answerD : "Doing his own thing, stop asking"
+// var questions = {
 
-}
+
+//     question1 :  "Where is Waldo currently?",
+//     answerA : "The Arctic",
+//     answerB : "Saturn",
+//     answerC : "The Death Star",
+//     answerD : "Doing his own thing, stop asking",
+
+//     correctAnswer = answerD,
+
+//     question2 : "Who is the strongest?"
+
+
+// }
+
+
+var checkAnswer = function() {
+
+    var radios = document.getElementsByName('answer1');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+    
+    if (val == "" ) {
+      alert('please an answer');
+    } else if ( val == "Doing his own thing. stop asking" ) {
+      alert('Your answer is correct!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
