88deec7c6d6203a62001b414039db24c56e47e54
Luke Klymshyn <unseenmountain@gmail.com>
2019-11-06 21:38:43 -0800

    Compleatly changed my game the start button now takes you to the first question also I added my own css along with bootstrap for personal flair

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,28 +12,103 @@
 
 //     correctAnswer = answerD,
 
-//     question2 : "Who is the strongest?"
+//     question2 : "Who is the strongest?",
+//   answerA : "Goku",
+//   answerB : "Saitama",
+//   answerC : "Jedi Reavan",
+//   answerD : "A Vault hunter",
+//   answerE : "All of them",
 
-
+//   correctAnswer : ""
 // }
 
+const startButton = document.getElementById("start");
+const questionContainer = document.getElementById("question-container");
 
-var checkAnswer = function() {
+startButton.addEventListener("click", startGame);
 
-    var radios = document.getElementsByName('answer1');
-    var val= "";
-    for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-           val = radios[i].value; 
-           break;
-         }
-    }
+function startGame(){
+  console.log("statted")
+  startButton.classList.add("hide");
+  questionContainer.classList.remove("hide")
+
+}
+
+
+
+
+// var checkAnswer1 = function() {
+
+//     var radios = document.getElementsByName('answer1');
+//     var val= "";
+//     for (var i = 0, length = radios.length; i < length; i++) {
+//         if (radios[i].checked) {
+//            val = radios[i].value; 
+//            break;
+//          }
+//     }
     
-    if (val == "" ) {
-      alert('please an answer');
-    } else if ( val == "Doing his own thing. stop asking" ) {
-      alert('Your answer is correct!');
-    } else {
-      alert('Answer is wrong');
-    }
-  };
+//     if (val == "" ) {
+//       if(val == "Doing his own thing. stop asking");
+//     } else if ( val == "Doing his own thing. stop asking" ) {
+//       alert('Your answer is correct!');
+//     } else {
+//       alert('Answer is wrong');
+//     }
+//   }
+
+//     var checkAnswer2 = function() {
+//     var radios = document.getElementsByName('answer2');
+//     var val= "";
+//     for (var i = 0, length = radios.length; i < length; i++) {
+//         if (radios[i].checked) {
+//            val = radios[i].value; 
+//            break;
+//          }
+//     }
+    
+//     if (val == "" ) {
+//       if(val == "All of them");
+//     } else if ( val == "All of them" ) {
+//       alert('Your answer is correct!');
+//     } else {
+//       alert('Answer is wrong');
+//     }
+//   };
+
+//   var checkAnswer3 = function(){
+//     var radios = document.getElementsByName('answer3');
+//     var val= "";
+//     for (var i = 0, length = radios.length; i < length; i++) {
+//         if (radios[i].checked) {
+//            val = radios[i].value; 
+//            break;
+//          }
+//     }
+    
+//     if (val == "" ) {
+//       if(val == "Snoop Dog");
+//     } else if ( val == "Snoop Dog" ) {
+//       alert('Your answer is correct!');
+//     } else {
+//       alert('Your answer is wrong');
+//     }
+//   };
+//   var checkAnswer4 = function(){
+//     var radios = document.getElementsByName('answer3');
+//     var val= "";
+//     for (var i = 0, length = radios.length; i < length; i++) {
+//         if (radios[i].checked) {
+//            val = radios[i].value; 
+//            break;
+//          }
+//     }
+    
+//     if (val == "" ) {
+//       if(val == "All of the above");
+//     } else if ( val == "Snoop Dog" ) {
+//       alert('Your answer is correct!');
+//     } else {
+//       alert('Your answer is wrong');
+//     }
+//   };
